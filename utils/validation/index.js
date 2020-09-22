210d83b08e220b68f4ac5ed1ca6262a2facd61ce
Eugenia Petrunia <avepetrunia@gmail.com>
2020-09-22 18:37:43 +0300

    Create validation schemas

---
+++ utils/validation/index.js
@@ -0,0 +1,30 @@
+const yup = require('yup');
+
+module.exports.CREATE_USER_SCHEMA = yup.object().shape({
+  firstName: yup.string().trim().required(),
+  lastName: yup.string().trim().required(),
+  email: yup.string().email().required(),
+  age: yup.number().positive().integer().max(120).required(),
+  gender: yup.string().oneOf(['male', 'female', 'other']).required(),
+  Password: yup
+    .string()
+    .matches(
+      /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,25}$/,
+      'password must be at least 8 characters long, be of mixed case and also contain a digit or symbol'
+    )
+    .required(),
+});
+
+module.exports.UPDATE_USER_SCHEMA = yup.object().shape({
+  firstName: yup.string().trim(),
+  lastName: yup.string().trim(),
+  email: yup.string().email(),
+  age: yup.number().positive().integer().max(120),
+  gender: yup.string().oneOf(['male', 'female', 'other']),
+  Password: yup
+    .string()
+    .matches(
+      /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,25}$/,
+      'password must be at least 8 characters long, be of mixed case and also contain a digit or symbol'
+    ),
+});
